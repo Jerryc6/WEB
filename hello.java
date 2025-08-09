da4f38188bc8c0e2b40a9aa301779cc442679b37
y0ur_$h@dow;-) <187157000+Jerryc6@users.noreply.github.com>
2025-08-09 11:57:58 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,16 @@
+import javax.swing.JFrame;
+import javax.*;
+
+
 public class hello{
+  public void window(){
+    
+  }
+
+  
   public static void main(String args[]){
     System.out.println("Hello");
+    window w = new window();
   }
   
 }
